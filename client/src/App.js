e5f669fc69ac4fd467c40580648b78496d1b8e4a
Ezequiel Etchevest <e.etchevest@hotmail.com>
2022-11-11 14:42:45 -0300

    SideBar

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,19 @@
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 import LogIn from './views/Log-in';
+import Home from './views/Home';
+import NavBar from './components/navBar';
 
 function App() {
   return (
     <ChakraProvider>
     <BrowserRouter>
     <div className="App">
-      <Switch>
+      <NavBar/>
+      <Routes>
       <Route path="/sign-in" element={<LogIn/>} />
-      </Switch> 
+      <Route path="/home" element={<Home/>} />
+      </Routes> 
     </div>
     </BrowserRouter>
     </ChakraProvider>
---
+++ client/src/components/navBar.js
@@ -0,0 +1,31 @@
+import {
+	Box,
+    Image,
+    Flex,
+    Spacer
+  } from '@chakra-ui/react';
+import logo from '../assets/NaturalistoneLogo.png'
+
+const NavBar = () => {
+
+	return (
+    <Flex	
+      pos={'fixed'}
+      bg={'white'}
+      w='100vw'
+      h='8vh'
+      zIndex={'1'}>
+
+        <Spacer/>
+        <Box
+          h={'8vh'}
+          w={'16vw'}
+          display={'flex'}>
+            <Image src={logo}  objectFit={'contain'}/>
+        </Box>
+
+    </Flex>
+	);
+}
+
+  export default NavBar;
