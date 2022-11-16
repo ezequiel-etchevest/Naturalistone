5b1e7bda274a82a8e9b8d4439eef5fff89cee538
Ezequiel Etchevest <e.etchevest@hotmail.com>
2022-11-16 00:19:51 -0300

    home localstore changes

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -64,7 +64,6 @@ export function getInvoicesBySeller(id){
             })
         }catch(error){
             console.log({error})           
-
         }
     }
 }
--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -5,41 +5,52 @@ import HomeContainer from "../components/homeContainer";
 import InfoContainer from "../components/infoContainer";
 import Stats from "../components/Stats";
 import { useDispatch, useSelector } from "react-redux";
-import { getInvoicesBySeller } from "../redux/actions";
+import { getInvoicesBySeller, getEmployeeById } from "../redux/actions";
 
 
 const Home = () => {
 
-  const user = JSON.parse(localStorage.getItem('user'))
+  const userLocal = JSON.parse(localStorage.getItem('user'))
   const [site, setSite] = useState('home')
   const dispatch = useDispatch()
+  
   const seller_invoices = useSelector(state => state.seller_invoices)
+  const user = useSelector(state => state.user)
 
   function handleSite(site){
     if(site === 'Home') return(<HomeContainer/>)
     if(site === 'Invoices') return(<InfoContainer seller_invoices={seller_invoices}/>)
     if(site === 'Stats') return (<Stats/>)
   }
-  console.log(seller_invoices)
-  useEffect(()=>{
-    if(seller_invoices.length < 0){
-    dispatch(getInvoicesBySeller(user.SellerID))}
-  },[seller_invoices])
 
-  return(
+
+  useEffect(()=>{
+    if(user){
+      if(user.length){
+        dispatch(getInvoicesBySeller(user.SellerID))
+      }
+    }
+    },[dispatch])
+
+  useEffect(()=>{
+    if(userLocal){
+      dispatch(getEmployeeById(userLocal.SellerID))}
+    },[])
+
+if(user){
+  if(Object.entries(user).length){
+    return(
+      <>
+        <SideBar user={user} site={site} setSite={setSite}/>
+        <Box>{handleSite(site)}</Box>
+      </>
+  )} else return(
     <>
-    { !Object.entries(user).length ? (
-    <>
-    <Text>Loading...</Text> 
-    </>
-    ) : (
-    <>
-    <SideBar user={user} site={site} setSite={setSite}/>
-    <Box>{handleSite(site)}</Box>
-    </>
-    )}
+      <Text>Loading...</Text> 
     </>
   )
 }
+}
+
 
 export default Home
