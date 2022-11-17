5b567f152c5aa389d4c28a8d4873feea27dd26cb
AritaRuv <ariadnaruvini@hotmail.com.ar>
2022-11-17 16:18:08 -0300

    controller string

--- api/src/Controllers/LastWeel.js
+++ api/src/Controllers/LastWeel.js
@@ -21,23 +21,23 @@ export default function LastWeek(array){
           if(actualDate.day > 8 ) return actualDate.day - 7
           else{
             if(actualDate.month === 1   && actualDate.month === 5 && actualDate.month === 7 && actualDate.month === 8 && actualDate.month === 10 && actualDate.month === 12) {
-                let day = 31 - (actualDate.day - 7)
+                let day = 31 + (actualDate.day - 7)
                 return day
             }
             if(actualDate.month === 4   && actualDate.month === 6 && actualDate.month === 9 && actualDate.month === 11 ) {
-                let day = 30 - (actualDate.day - 7)
+                let day = 30 + (actualDate.day - 7)
                 return day
             }
             if(actualDate.month === 2) {
                 if(actualDate.month % 4 == 0 && year % 100 != 0 || year % 400 == 0){
-                    let day = 29 - (actualDate.day - 7)
+                    let day = 29 + (actualDate.day - 7)
                     return day
                 }else{
-                    let day = 28 - (actualDate.day - 7)
+                    let day = 28 + (actualDate.day - 7)
                     return day
                 }      
             }
-          }}}
+          }}
         let limitMonth = () =>{
             if(actualDate.day < 8 ){
                 if (actualDate.month <= 1 ) return 12
@@ -54,7 +54,7 @@ export default function LastWeek(array){
         let month = limitMonth()
         let year = limitYear()
 
-        return {year, day, month}
+        return `${year}-${day}-${month}T00:00:00.000Z`}
     
     function compareDate(a, b ){
         
