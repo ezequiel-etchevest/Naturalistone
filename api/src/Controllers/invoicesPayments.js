8d06a2e5ee2dd17e5f3103ec0f173e9628888c3f
AritaRuv <ariadnaruvini@hotmail.com.ar>
2022-11-29 00:58:19 -0300

    payment routes

--- api/src/Controllers/invoicesPayments.js
+++ api/src/Controllers/invoicesPayments.js
@@ -1,17 +1,23 @@
 
 
 
-const invoicesPayments = (simple, repetida) => {
-    // let nArray = simple.map(i => {
-    //     return  i.payments = repetida.filter(p => p.InvoiceID == i.Naturali_Invoice)
-    // })
-    // return nArray
+const invoicesPayments = (array) => {
 
+    array.forEach(invoices => {
+        if( invoices.Payments){
+            invoices.Payments = invoices.Payments.split(',')
+            invoices.Payments = invoices.Payments.map(e => {
+                e = e.split(';')
+                e[2] = e[2].split(' ')[0]
+                return e
+            })
+        } });
+
+    return array
 }
 
-
-
-
-
-
 module.exports = invoicesPayments
+
+
+
+
