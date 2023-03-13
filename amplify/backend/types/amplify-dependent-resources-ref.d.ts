92e7e0438a0ff2408a2a15336779bfa02e9fb3ae
AritaRuv <ariadnaruvini@hotmail.com.ar>
2023-03-13 20:20:59 -0300

    aws amplofy

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,18 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "NaturaliRestAPI": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "naturalistoneapp117a377a": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
