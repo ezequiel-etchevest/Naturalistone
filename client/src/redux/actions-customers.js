27a80205c53a5113c948b61b8e9dab5e7a278e2c
Ezequiel Etchevest <e.etchevest@hotmail.com>
2023-04-20 11:05:39 +0200

    phase 2 routes

---
+++ client/src/redux/actions-customers.js
@@ -0,0 +1,57 @@
+import axios from 'axios';
+
+export const GET_CUSTOMERS = 'GET_CUSTOMERS';
+export const GET_CUSTOMERS_BY_ID = 'GET_CUSTOMERS_BY_ID';
+export const POST_CUSTOMER = 'POST_CUSTOMER';
+
+
+export function getCustomers(){
+  
+    return async function(dispatch){
+        try{ 
+
+            let {data} = await axios.get(`/customers`)
+            dispatch(
+            {
+                type: GET_CUSTOMERS,
+                payload: data
+            })
+        }catch(error){
+            console.log({error})
+        }
+    }
+}
+
+export function getCustomersById(customerId){
+
+    return async function(dispatch){
+        try{ 
+
+            let {data} = await axios.get(`/customers/${customerId}`)
+            dispatch(
+            {
+                type: GET_CUSTOMERS_BY_ID,
+                payload: data
+            })
+        }catch(error){
+            console.log({error})
+        }
+    }
+}
+
+export function createCustomer(customerDetails){
+
+  return async function(dispatch){
+      try{
+          let { data } = await axios.post(`/customer`, customerDetails)
+
+              dispatch(
+              {
+                  type: POST_CUSTOMER,
+                  payload: data
+              })
+
+      }catch(error){
+          console.log({error})           
+      }}
+    }
