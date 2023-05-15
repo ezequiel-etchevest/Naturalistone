a568f5a7f060b73f53bb5b74e2e7ff0f79a76c14
AritaRuv <ariadnaruvini@hotmail.com.ar>
2023-05-15 12:25:16 +0200

    imgs

--- client/src/components/products/miniCarousel.js
+++ client/src/components/products/miniCarousel.js
@@ -4,6 +4,11 @@ import "slick-carousel/slick/slick-theme.css";
 import { Box } from '@chakra-ui/react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useEffect, useState } from 'react';
+import Picture1 from '../../assets/ProductPicture/354-1.jpg'
+import Picture2 from '../../assets/ProductPicture/354-2.jpg'
+import Picture3 from '../../assets/ProductPicture/354-3.jpg'
+import Picture4 from '../../assets/ProductPicture/354-4.jpg'
+import Picture5 from '../../assets/ProductPicture/354-5.jpg'
 
 const CarouselProduct = () => {
   const dispatch = useDispatch();
@@ -31,11 +36,40 @@ const CarouselProduct = () => {
   return (
     <Box px="1vh">
       <Slider {...settings}>
-        {images.map((url, i) => (
-          <Box key={i} p="1vh" h="23vh">
-            <img src={url} alt={`Image ${i}`} />
-          </Box>
-        ))}
+        { images.length ? (
+           images.map((url, i) => (
+            <Box key={i} p="1vh" h="23vh">
+              <img
+                src={url}
+                alt={`Image ${i}`}
+                style={{ height: '100%', objectFit: 'cover' }} />
+            </Box>
+          ))
+        ) : (
+          <>
+            <Box  px={'1vh'}  >
+            <Slider {...settings}>
+              <Box p={'1vh'} h={'23vh'}>
+                <img src={Picture1} />
+              </Box>
+              <Box p={'1vh'} h={'23vh'}>
+                <img src={Picture2} />
+              </Box >
+              <Box p={'1vh'} h={'23vh'}>
+                <img src={Picture3} />
+              </Box>
+              <Box p={'1vh'} h={'23vh'}>
+                <img src={Picture4} />
+              </Box>
+              <Box p={'1vh'} h={'23vh'}>
+                <img src={Picture5} />
+              </Box>
+      
+            </Slider>
+            </Box>
+          </>
+          )
+       }
       </Slider>
     </Box>
   );
