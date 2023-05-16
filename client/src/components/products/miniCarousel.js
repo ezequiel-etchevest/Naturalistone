cada50075e9a564fdc6455e1680d2c2145826398
AritaRuv <ariadnaruvini@hotmail.com.ar>
2023-05-16 13:44:52 +0200

    modal

--- client/src/components/products/miniCarousel.js
+++ client/src/components/products/miniCarousel.js
@@ -1,13 +1,15 @@
 import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { Box } from '@chakra-ui/react';
+import { Box, useDisclosure, Modal, ModalOverlay, ModalContent, ModalCloseButton, ModalBody } from '@chakra-ui/react';
 import { useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
+import img1 from '../../assets/ProductPicture/354-1.jpg'
 
 
 const CarouselProduct = () => {
 
+  const { isOpen, onOpen, onClose } = useDisclosure()
   const productImages = useSelector(state => state.product_images);
   const [images, setImages] = useState([]);
   
@@ -22,19 +24,37 @@ const CarouselProduct = () => {
     fetchImages();
   }, [productImages]);
 
+
   const settings = {
     infinite: true,
     speed: 500,
     slidesToShow: images.length === 1 ? 1 : 2,
+    //slidesToShow: 2,
     slidesToScroll: 1,
     adaptiveHeight: false,
   };
   
-  console.log(images)
   return (
+    <>
     <Box px="1vh">
       <Slider {...settings}>
-        {
+        <Box p="1vh" h="23vh">
+          <img
+          onClick={onOpen}
+          src={img1}
+          alt={`Image `}
+          style={{ height: '100%', objectFit: 'cover', margin: '0 auto' }}
+          />
+        </Box>
+        <Box p="1vh" h="23vh">
+          <img
+          onClick={onOpen}
+          src={img1}
+          alt={`Image `}
+          style={{ height: '100%', objectFit: 'cover', margin: '0 auto' }}
+          />
+        </Box>
+        {/* {
            images.map((url, i) => (
             <Box key={i} p="1vh" h="23vh">
               <img
@@ -44,9 +64,65 @@ const CarouselProduct = () => {
                 />
             </Box>
           ))
-       }
+       } */}
       </Slider>
+
     </Box>
+          <Modal isOpen={isOpen} onClose={onClose}  size={'6xl'}>
+          <ModalOverlay />
+          <ModalContent
+            rounded={'md'} 
+            mt={'4vh'} 
+            mb={'4vh'} 
+            w={'60vw'} 
+            bg={'web.sideBar'} 
+            border={'1px solid'} 
+            pt={'2vh'} 
+            pb={'2vh'} 
+            borderColor={'web.border'}>
+            <ModalCloseButton />
+            <ModalBody w={'100%'} h={'100%'}>
+              <Box>
+                <Slider {...settings}>
+                  {/* <Box px='0.5vw'>
+                    <img
+                      src={img1}
+                      alt={`Image `}
+                      style={{ height: '100%', objectFit: 'cover', margin: '0 auto' }}
+                    />
+                  </Box>
+                  <Box px='0.5vw'>
+                    <img
+                      src={img1}
+                      alt={`Image `}
+                      style={{ height: '100%', objectFit: 'cover', margin: '0 auto' }}
+                    />
+                  </Box>
+                  <Box px='0.5vw'>
+                    <img
+                      src={img1}
+                      alt={`Image `}
+                      style={{ height: '100%', objectFit: 'cover', margin: '0 auto' }}
+                    />
+                  </Box> */}
+                  {
+                    images.map((url, i) => (
+                      <Box key={i} p="1vh" h="23vh">
+                        <img
+                          src={url}
+                          alt={`Image ${i}`}
+                          style={{ height: '100%', objectFit: 'cover' }} 
+                          />
+                      </Box>
+                      ))
+                    }
+                  </Slider>
+
+              </Box>
+            </ModalBody>
+          </ModalContent>
+        </Modal>
+      </>
   );
 };
 
