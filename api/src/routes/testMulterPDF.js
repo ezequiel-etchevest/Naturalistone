7891c8612e1ba8595f2efe3bb853db8a54d35dec
Ezequiel Etchevest <e.etchevest@hotmail.com>
2023-05-30 17:59:18 +0200

    test pdf write

--- api/src/routes/testMulterPDF.js
+++ api/src/routes/testMulterPDF.js
@@ -1,58 +1,34 @@
 const express = require('express');
 const testRouter = express.Router();
-const mysqlConnection = require('../db');
+const multer = require('multer');
+const path = require('path');
 
-testRouter.post('/pdf', async function (req, res) {
 
-//   const pdf = req.files.pdf; 
+const invoicePath = path.join('/app/OneDrive', 'Naturali', 'InvoicesReceived', 'Parsed Documents', 'Invoice Naturali');
+// Configurar multer para manejar la carga de archivos
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, invoicePath); // Especifica la carpeta de destino donde deseas guardar el archivo PDF
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname); // Especifica el nombre del archivo PDF
+  },
+});
 
-console.log(req.files.pdf)
-//   if (!pdf) {
-//     res.status(400).json({ estado: false, mensaje: 'No se proporcionó ningún archivo PDF' });
-//     return;
-//   }
+const upload = multer({ storage });
 
-//   try {
-//     // Aquí puedes guardar el archivo PDF en la ubicación deseada, como una carpeta en el servidor o un servicio de almacenamiento en la nube
-//     // Puedes utilizar el módulo 'fs' de Node.js o una librería de almacenamiento en la nube como 'aws-sdk' o 'google-cloud-storage'
+testRouter.post('/', upload.single('pdfFile'), async function (req, res) {
+  try {
+    console.log(req.file); // Verificar el objeto file recibido
 
-//     // Ejemplo: Guardar el archivo PDF en una carpeta local
-//     const filePath = 'C:\Users\Keki\Desktop\Naturalistone\Naturalistone' + pdf.name;
-//     pdf.mv(filePath, function (error) {
-//       if (error) {
-//         console.error('Error al guardar el archivo PDF:', error);
-//         res.status(500).json({ estado: false, mensaje: 'Error al guardar el archivo PDF' });
-//       } else {
-//         console.log('Archivo PDF guardado correctamente');
-//         res.status(200).json({ estado: true, mensaje: 'Archivo PDF guardado correctamente' });
-//       }
-//     });
-//   } catch (error) {
-//     console.error('Error al guardar el archivo PDF:', error);
-//     res.status(500).json({ estado: false, mensaje: 'Error al guardar el archivo PDF' });
-//   }
+    const filePath = req.file.path; // Obtener la ruta del archivo guardado
+
+    console.log('PDF guardado en el servidor correctamente en:', filePath);
+    res.status(200).send('PDF guardado en el servidor.');
+  } catch (error) {
+    console.error('Error al guardar el PDF en el servidor:', error);
+    res.status(500).send('Error al guardar el PDF en el servidor.');
+  }
 });
 
 module.exports = testRouter;
-
-
-const express = require('express');
-const app = express();
-const fs = require('fs-extra');
-
-app.post('/api/save-pdf', (req, res) => {
-  const pdfBuffer = req.body;
-
-  // Especifica la ruta de destino donde deseas guardar el PDF en el servidor
-  const destinationPath = '/ruta/de/destino/nombre_archivo.pdf';
-
-  fs.writeFile(destinationPath, pdfBuffer, (error) => {
-    if (error) {
-      console.error('Error al guardar el PDF en el servidor:', error);
-      res.status(500).send('Error al guardar el PDF en el servidor.');
-    } else {
-      console.log('PDF guardado en el servidor correctamente.');
-      res.status(200).send('PDF guardado en el servidor.');
-    }
-  });
-});
--- client/src/components/invoices/createQuote/createQuotePdf.js
+++ client/src/components/invoices/createQuote/createQuotePdf.js
@@ -4,193 +4,141 @@ import { PDFDocument, rgb } from 'pdf-lib';
 import { Box, Center, Spinner } from '@chakra-ui/react';
 import axios from 'axios';
 
-// const CreatedQuotePdf = ({variables, user, customer, project }) => {
+const CreatedQuotePdf = ({variables, user, customer, project }) => {
 
 
-//     const posted_quote = useSelector(state => state.posted_quote)
+    const posted_quote = useSelector(state => state.posted_quote)
  
-//     const [pdfInfo, setPdfInfo] = useState([]);
-//     const viewer = useRef(null);
-//     const mappedProducts =  posted_quote.parsedProducts
+    let invoiceID = posted_quote.Naturali_Invoice
+    const date = posted_quote.InsertDate
+    const [pdfInfo, setPdfInfo] = useState([]);
+    const viewer = useRef(null);
+    const mappedProducts =  posted_quote.parsedProducts
 
-//     useEffect(() => {
-//         CreateForm();
-//     }, [posted_quote]);
+    useEffect(() => {
+        CreateForm();
+    }, [posted_quote]);
 
-//     async function waitUntilMappedProductsExists() { //esta promesa espera que se llene el estado de 
-//       return new Promise((resolve) => {              //posted_quote para poder captar los productos correctamente
-//         const checkMappedProducts = () => {
-//           if (Array.isArray(mappedProducts)) {
-//             resolve();
-//           } else {
-//             setTimeout(checkMappedProducts, 100); // Esperar 100ms antes de volver a verificar
-//           }
-//         };
+    async function waitUntilMappedProductsExists() { //esta promesa espera que se llene el estado de 
+      return new Promise((resolve) => {              //posted_quote para poder captar los productos correctamente
+        const checkMappedProducts = () => {
+          if (Array.isArray(mappedProducts)) {
+            resolve();
+          } else {
+            setTimeout(checkMappedProducts, 100); // Esperar 100ms antes de volver a verificar
+          }
+        };
     
-//         checkMappedProducts();
-//       });
-//     }
-
-//  async function CreateForm() {
-
-//   const url = `/Quote/quote-blank.pdf`
-//   const existingPdfBytes = await fetch(url).then((res) => res.arrayBuffer());
-//   var bytes = new Uint8Array(existingPdfBytes);
-//   const pdfDoc = await PDFDocument.load(bytes);
-
-//   const pages = pdfDoc.getPages();
-//   const page = pages[0];
-
-//   const invoiceID = posted_quote.Naturali_Invoice
-//   const date = posted_quote.InsertDate
-
-//   var subtotal = 0
-
-//   let y = 460.8;
-
-//   const name = customer.Contact_Name
-//   const phone = customer.Phone
-//   const email = customer.Email
-//   const street = project.Shipping_Address
-//   const city = project.City
-//   const state = project.State
-//   const zipCode = project.ZipCode
-//   const company = customer.Company
-//   const PO = variables.method
-//   const ref = user[0].SellerReference
-//   const tax = 7
-//   const estDate = variables.estDelivDate
-//   const deliveryMethod = variables.shipVia
-//   const paymentTerms = variables.paymentTerms
-
-//   const streetCustomer = customer.Address
-//   const cityCustomer = customer.City
-//   const stateCustomer = customer.State
-//   const zipCodeCustomer = customer.ZipCode
-
-// //   page.drawText(`${no}`, { x: 472, y: 666, size: 16, color: rgb(1, 0.3, 0) })
-//   page.drawText(`${date}`, { x: 448, y: 688, size: 10 })
-//   page.drawText(`${invoiceID}`, { x: 514, y: 687, size: 12, color: rgb(1, 0.3, 0)})
-
-//   page.drawText(`${name}`, { x: 42, y: 626, size: 10 })
-//   page.drawText(`${phone}`, { x: 42, y: 612, size: 10 })
-//   page.drawText(`${email}`, { x: 42, y: 598, size: 10 })
-
-//   page.drawText(street, { x: 336, y: 626, size: 10 })
-//   page.drawText(`${city }, ${state}`, { x: 336, y: 612, size: 10 })
-//   page.drawText(zipCode, { x: 336, y: 598, size: 10 })
-
-//   page.drawText(`${company}`, { x: 40, y: 508, size: 10 })
-//   page.drawText(`${PO}`, { x: 162, y: 508, size: 10 })
-//   page.drawText(`${ref}`, { x: 272, y: 508, size: 10 })
-//   page.drawText(`${estDate}`, { x: 354, y: 508, size: 10 }) 
-//   page.drawText(`${deliveryMethod}`, { x: 439, y: 508, size: 10 }) 
-//   page.drawText(`${paymentTerms}`, { x: 524, y: 508, size: 10 }) 
-
-
-// // //This line uses the forEach method to iterate over each key-value pair in the array created by Object.
-// // //entries. For each iteration, the key (variableName) and value (element) are destructured from the pair and
-// // //passed as arguments to the callback function.
-
-// await waitUntilMappedProductsExists();
-
-// mappedProducts.forEach((product, index) => {
-//   const { variableName } = product;
-
-//   subtotal = subtotal + product.price * product.quantity
-
-//   page.drawText(`${product.quantity}`, { x: 46, y, size: 10 });
-//   page.drawText(`Units`, { x: 76, y, size: 10 });
-//   page.drawText(`${product.prodID}`, { x: 134, y, size: 10 });
-//   page.drawText(`${product.prodName} - ${product.finish}`, { x: 210, y, size: 10 });
-//   page.drawText(`${product.price}`, { x: 470, y, size: 10 });
-//   page.drawText(`${product.price * product.quantity}`, { x: 528, y, size: 10 });
-//   y -= 14;
-// });
-
-//   page.drawText(`${subtotal}`, { x: 528, y: 272, size: 10 });
-//   page.drawText(`(${tax} %)`, { x: 454, y: 249, size: 12 });
-//   page.drawText(`${(subtotal * tax / 100).toFixed(2)}`, { x: 528, y: 247, size: 10 });
-//   page.drawText(`$ ${(subtotal + (subtotal * tax / 100)).toFixed(2)}`, { x: 510, y: 222, size: 12 });
-
-
-//   const pdfBytes = await pdfDoc.save();
-//   const blob = new Blob([pdfBytes], { type: 'application/pdf' });
-  
-//   setPdfInfo(URL.createObjectURL(blob));
-
-  
-
-//   };
-//   return (
-//   <>
-//     {
-//       Object.entries(posted_quote).length ?
-//         posted_quote.Naturali_Invoice && posted_quote.InsertDate ?
-//         <Box h={'85vh'} >
-//           {<iframe width={'100%'} height={'100%'} title="quote-blank" src={pdfInfo} ref={viewer} type="application/pdf" />}
-//         </Box>
-//         :
-//         <Center ml={'16vw'} w={'84vw'} bg={'web.bg'} h={'92vh'}>
-//           <Spinner thickness={'4px'} size={'xl'} color={'logo.orange'}/>
-//         </Center>
-//       :
-//       <Center ml={'16vw'} w={'84vw'} bg={'web.bg'} h={'92vh'}>
-//         <Spinner thickness={'4px'} size={'xl'} color={'logo.orange'}/>
-//       </Center>
-//     }
-//   </>
-//   )
-//   };
-const CreatedQuotePdf = ({ variables, user, customer, project }) => {
-  const posted_quote = useSelector((state) => state.posted_quote);
-  const [pdfInfo, setPdfInfo] = useState([]);
-  const viewer = useRef(null);
-  const mappedProducts = posted_quote.parsedProducts;
-
-  useEffect(() => {
-    CreateForm();
-  }, [posted_quote]);
-
-  async function waitUntilMappedProductsExists() {
-    return new Promise((resolve) => {
-      const checkMappedProducts = () => {
-        if (Array.isArray(mappedProducts)) {
-          resolve();
-        } else {
-          setTimeout(checkMappedProducts, 100);
-        }
-      };
-
-      checkMappedProducts();
-    });
-  }
-
-  async function CreateForm() {
-    const url = `/Quote/quote-blank.pdf`;
-    const existingPdfBytes = await axios.get(url, {
-      responseType: 'arraybuffer',
-    });
-    const pdfDoc = await PDFDocument.load(existingPdfBytes.data);
-
-    // Resto del código para generar el PDF
-
-    const pdfBytes = await pdfDoc.save();
-    const blob = new Blob([pdfBytes], { type: 'application/pdf' });
-
-    setPdfInfo(URL.createObjectURL(blob));
-
-    savePdfOnServer(pdfBytes);
-  }
-
-  async function savePdfOnServer(pdfBytes) {
-    try {
-      const response = await axios.post('/api/save-pdf', pdfBytes, {
-        headers: {
-          'Content-Type': 'application/pdf',
-        },
+        checkMappedProducts();
       });
+    }
 
+ async function CreateForm() {
+
+  const url = `/Quote/quote-blank.pdf`
+  const existingPdfBytes = await fetch(url).then((res) => res.arrayBuffer());
+  var bytes = new Uint8Array(existingPdfBytes);
+  const pdfDoc = await PDFDocument.load(bytes);
+
+  const pages = pdfDoc.getPages();
+  const page = pages[0];
+
+
+
+  var subtotal = 0
+
+  let y = 460.8;
+
+  const name = customer.Contact_Name
+  const phone = customer.Phone
+  const email = customer.Email
+  const street = project.Shipping_Address
+  const city = project.City
+  const state = project.State
+  const zipCode = project.ZipCode
+  const company = customer.Company
+  const PO = variables.method
+  const ref = user[0].SellerReference
+  const tax = 7
+  const estDate = variables.estDelivDate
+  const deliveryMethod = variables.shipVia
+  const paymentTerms = variables.paymentTerms
+
+  const streetCustomer = customer.Address
+  const cityCustomer = customer.City
+  const stateCustomer = customer.State
+  const zipCodeCustomer = customer.ZipCode
+
+//   page.drawText(`${no}`, { x: 472, y: 666, size: 16, color: rgb(1, 0.3, 0) })
+  page.drawText(`${date}`, { x: 448, y: 688, size: 10 })
+  page.drawText(`${invoiceID}`, { x: 514, y: 687, size: 12, color: rgb(1, 0.3, 0)})
+
+  page.drawText(`${name}`, { x: 42, y: 626, size: 10 })
+  page.drawText(`${phone}`, { x: 42, y: 612, size: 10 })
+  page.drawText(`${email}`, { x: 42, y: 598, size: 10 })
+
+  page.drawText(street, { x: 336, y: 626, size: 10 })
+  page.drawText(`${city }, ${state}`, { x: 336, y: 612, size: 10 })
+  page.drawText(zipCode, { x: 336, y: 598, size: 10 })
+
+  page.drawText(`${company}`, { x: 40, y: 508, size: 10 })
+  page.drawText(`${PO}`, { x: 162, y: 508, size: 10 })
+  page.drawText(`${ref}`, { x: 272, y: 508, size: 10 })
+  page.drawText(`${estDate}`, { x: 354, y: 508, size: 10 }) 
+  page.drawText(`${deliveryMethod}`, { x: 439, y: 508, size: 10 }) 
+  page.drawText(`${paymentTerms}`, { x: 524, y: 508, size: 10 }) 
+
+
+// //This line uses the forEach method to iterate over each key-value pair in the array created by Object.
+// //entries. For each iteration, the key (variableName) and value (element) are destructured from the pair and
+// //passed as arguments to the callback function.
+
+await waitUntilMappedProductsExists();
+
+mappedProducts.forEach((product, index) => {
+  const { variableName } = product;
+
+  subtotal = subtotal + product.price * product.quantity
+
+  page.drawText(`${product.quantity}`, { x: 46, y, size: 10 });
+  page.drawText(`Units`, { x: 76, y, size: 10 });
+  page.drawText(`${product.prodID}`, { x: 134, y, size: 10 });
+  page.drawText(`${product.prodName} - ${product.finish}`, { x: 210, y, size: 10 });
+  page.drawText(`${product.price}`, { x: 470, y, size: 10 });
+  page.drawText(`${product.price * product.quantity}`, { x: 528, y, size: 10 });
+  y -= 14;
+});
+
+  page.drawText(`${subtotal}`, { x: 528, y: 272, size: 10 });
+  page.drawText(`(${tax} %)`, { x: 454, y: 249, size: 12 });
+  page.drawText(`${(subtotal * tax / 100).toFixed(2)}`, { x: 528, y: 247, size: 10 });
+  page.drawText(`$ ${(subtotal + (subtotal * tax / 100)).toFixed(2)}`, { x: 510, y: 222, size: 12 });
+
+
+  const pdfBytes = await pdfDoc.save();
+  const blob = new Blob([pdfBytes], { type: 'application/pdf' });
+  
+  setPdfInfo(URL.createObjectURL(blob));
+
+  savePdfOnServer(pdfBytes, invoiceID);
+
+  };
+  
+  async function savePdfOnServer(pdfBytes, invoiceID) {
+    console.log(invoiceID)
+    try {
+      // Configurar el encabezado Content-Type para FormData
+      axios.interceptors.request.use(function (config) {
+        config.headers['Content-Type'] = 'multipart/form-data';
+        return config;
+      });
+  
+      const formData = new FormData();
+      formData.append('pdfFile', new Blob([pdfBytes], { type: 'application/pdf' }), `${invoiceID}.pdf`);
+      console.log('formData', formData);
+     
+      const response = await axios.post('/save-pdf', formData);
+  
       if (response.status === 200) {
         console.log('PDF guardado en el servidor correctamente.');
         // Realizar cualquier acción adicional después de guardar el PDF en el servidor
@@ -201,37 +149,28 @@ const CreatedQuotePdf = ({ variables, user, customer, project }) => {
       console.error('Error al realizar la solicitud al servidor:', error);
     }
   }
-
+  
+  
   return (
-    <>
-      {Object.entries(posted_quote).length ? (
-        posted_quote.Naturali_Invoice && posted_quote.InsertDate ? (
-          <Box h={'85vh'}>
-            {
-              <iframe
-                width={'100%'}
-                height={'100%'}
-                title="quote-blank"
-                src={pdfInfo}
-                ref={viewer}
-                type="application/pdf"
-              />
-            }
-          </Box>
-        ) : (
-          <Center ml={'16vw'} w={'84vw'} bg={'web.bg'} h={'92vh'}>
-            <Spinner thickness={'4px'} size={'xl'} color={'logo.orange'} />
-          </Center>
-        )
-      ) : (
+  <>
+    {
+      Object.entries(posted_quote).length ?
+        posted_quote.Naturali_Invoice && posted_quote.InsertDate ?
+        <Box h={'85vh'} >
+          {<iframe width={'100%'} height={'100%'} title="quote-blank" src={pdfInfo} ref={viewer} type="application/pdf" />}
+        </Box>
+        :
         <Center ml={'16vw'} w={'84vw'} bg={'web.bg'} h={'92vh'}>
-          <Spinner thickness={'4px'} size={'xl'} color={'logo.orange'} />
+          <Spinner thickness={'4px'} size={'xl'} color={'logo.orange'}/>
         </Center>
-      )}
-    </>
-  );
-};
-
+      :
+      <Center ml={'16vw'} w={'84vw'} bg={'web.bg'} h={'92vh'}>
+        <Spinner thickness={'4px'} size={'xl'} color={'logo.orange'}/>
+      </Center>
+    }
+  </>
+  )
+  };
 
 
 export default CreatedQuotePdf
