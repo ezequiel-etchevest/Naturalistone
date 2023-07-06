b17647e2999c7e6df3855a7a532df3a2828c2faf
Eduardo Sequeira <eduardoasm19@gmail.com>
2023-07-06 17:07:11 -0400

    changes samples and send email createquote

--- client/src/components/samples/createSample/CreateSampleProjects.js
+++ client/src/components/samples/createSample/CreateSampleProjects.js
@@ -1,22 +1,26 @@
 import {
-    Text,
-    Box,
-    HStack,
-    Input,
-    Tooltip,
-    Divider,
-    Select,
-    } from "@chakra-ui/react"
+  Text,
+  Box,
+  HStack,
+  Input,
+  Tooltip,
+  Divider,
+  Select,
+} from "@chakra-ui/react";
 import { useSelector } from "react-redux";
 import { useEffect } from "react";
 import { CreateNewProject } from "../../customers/customerDetail/createProject";
-import '../../../assets/styleSheet.css'
+import "../../../assets/styleSheet.css";
 import CreateSampleProjectList from "./CreateSampleProjectList";
 
-
-const CreateSampleProjects = ({ formData, setFormData, setDisable, errorsTrackingNumber, setErrorsTrackingNumber }) => {
-
-  const projects = useSelector(state => state.projects_by_customer_id)
+const CreateSampleProjects = ({
+  formData,
+  setFormData,
+  setDisable,
+  errorsTrackingNumber,
+  setErrorsTrackingNumber,
+}) => {
+  const projects = useSelector((state) => state.projects_by_customer_id);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -27,54 +31,96 @@ const CreateSampleProjects = ({ formData, setFormData, setDisable, errorsTrackin
         [name]: value,
       },
     });
-    console.log(formData.variables.trackingNumber)
+    console.log(formData.variables.trackingNumber);
   };
-  
-//   useEffect(() => {
-//     if(formData.variables.shipVia && formData.variables.method && formData.variables.paymentTerms && formData.variables.estDelivDate && formData.project.ProjectName){
-//       setDisable(false)
-//     } else { 
-//       setDisable(true)}
-// }, [formData.variables, formData.project]);
 
-  return(
+  //   useEffect(() => {
+  //     if(formData.variables.shipVia && formData.variables.method && formData.variables.paymentTerms && formData.variables.estDelivDate && formData.project.ProjectName){
+  //       setDisable(false)
+  //     } else {
+  //       setDisable(true)}
+  // }, [formData.variables, formData.project]);
 
-  <>
-    <Box color={'web.text2'} display={'flex'} justifyContent={'center'} flexDir={'column'} h={'58vh'}>
-      <Box mt={'10px'}>
-        <Text ml={'2vw'} mt={'2vh'} mb={'2vh'} fontSize={'lg'}w={'16vw'} color={'white'} alignSelf={'flex-start'}> 
-        Tracking
-        </Text>
-        <Text fontSize='sm' fontWeight={'semisemibold'} ml={'4.5vw'}> Tracking Number </Text>
-        <Input
-          mb={'0.5vh'}
-          w={'25vw'}
-          minH={'4.5vh'}
-          variant="unstyled"
-          textColor={'web.text2'}
-          _placeholder={{ fontFamily: 'body', fontWeight: 'inherit' }}
-          size={"sm"}
-          borderBottomWidth={"2px"}
-          borderBottomColor={'web.text2'}
-          type={"text"}
-          name={"trackingNumber"}
-          ml={'4.5vw'}
-          value={formData.variables.trackingNumber || ""}
-          onChange={(e)=>handleChange(e)}
+  return (
+    <>
+      <Box
+        color={"web.text2"}
+        display={"flex"}
+        justifyContent={"center"}
+        flexDir={"column"}
+        h={"58vh"}
+      >
+        <Box mt={"10px"}>
+          <Text
+            ml={"2vw"}
+            mt={"2vh"}
+            mb={"2vh"}
+            fontSize={"lg"}
+            w={"16vw"}
+            color={"white"}
+            alignSelf={"flex-start"}
+          >
+            Tracking
+          </Text>
+          <Text fontSize="sm" fontWeight={"semisemibold"} ml={"4.5vw"}>
+            {" "}
+            Tracking Number{" "}
+          </Text>
+          <Input
+            mb={"0.5vh"}
+            w={"25vw"}
+            minH={"4.5vh"}
+            variant="unstyled"
+            textColor={"web.text2"}
+            _placeholder={{ fontFamily: "body", fontWeight: "inherit" }}
+            size={"sm"}
+            borderBottomWidth={"2px"}
+            borderBottomColor={"web.text2"}
+            type={"text"}
+            name={"trackingNumber"}
+            ml={"4.5vw"}
+            value={formData.variables.trackingNumber || ""}
+            onChange={(e) => handleChange(e)}
           />
-            { errorsTrackingNumber.trackingNumber && (
-                <Text mt={'0.5vh'} position={'absolute'} color={'web.error'} fontSize={'xs'} ml={'5vw'}>
-                  {errorsTrackingNumber.trackingNumber}
-                </Text>
-            )}
+          {errorsTrackingNumber.trackingNumber && (
+            <Text
+              mt={"0.5vh"}
+              position={"absolute"}
+              color={"web.error"}
+              fontSize={"xs"}
+              ml={"5vw"}
+            >
+              {errorsTrackingNumber.trackingNumber}
+            </Text>
+          )}
+        </Box>
+        <Box display={"flex"} alignItems={"end"} h={"10vh"}>
+          <Box h={"9vh"}>
+            <Text
+              ml={"2vw"}
+              mt={"5vh"}
+              mb={"5vh"}
+              fontSize={"lg"}
+              w={"16vw"}
+              color={"white"}
+              alignSelf={"flex-start"}
+            >
+              Select Project
+            </Text>
+          </Box>
+          <Box display={"flex"} justifyContent={"flex-end"} w={"35vw"}>
+            <CreateNewProject customer={formData.customer} />
+          </Box>
+        </Box>
+        <CreateSampleProjectList
+          projects={projects}
+          formData={formData}
+          setFormData={setFormData}
+          setDisable={setDisable}
+        />
       </Box>
-      <Box display={'flex'} alignItems={'end'} h={'10vh'}>
-      <Text ml={'2vw'} mt={'5vh'} mb={'5vh'} fontSize={'lg'}w={'16vw'} color={'white'} alignSelf={'flex-start'}>Select Project</Text>
-      {/* <CreateNewProject customer={formData.customer} /> */}
-      </Box>
-        <CreateSampleProjectList projects={ projects } formData={formData} setFormData={setFormData} setDisable={setDisable}/>
-    </Box>
-  </>
-)}
+    </>
+  );
+};
 
-export default CreateSampleProjects
+export default CreateSampleProjects;
--- client/src/views/Samples.js
+++ client/src/views/Samples.js
@@ -1,55 +1,55 @@
 import React, { useEffect, useState } from "react";
 import SideBar from "../components/sideBar";
 import { useDispatch, useSelector } from "react-redux";
-import { getEmployeeById } from '../redux/actions-employees';
+import { getEmployeeById } from "../redux/actions-employees";
 import Redirect from "./RedirectPage";
 import { Text } from "@chakra-ui/react";
 import { useLocation } from "react-router-dom";
-import SamplesContainer from '../components/samples/SamplesContainer'
+import SamplesContainer from "../components/samples/SamplesContainer";
 import { getSamples } from "../redux/actions-samples";
 
-
-
-const Samples = ({focus, setFocus}) => {
-  
-  const dispatch = useDispatch()
-  const samples = useSelector(state => state.samples)
-  const user = useSelector(state => state.user)
-  const [focusFilter, setFocusFilter] = useState('All')
-  const userLocal = JSON.parse(localStorage.getItem('user'))
-
-  useEffect(()=>{
-    if(userLocal && !user.length){
-      dispatch(getEmployeeById(userLocal.SellerID))
-    }})
+const Samples = ({ focus, setFocus }) => {
+  const dispatch = useDispatch();
+  const samples = useSelector((state) => state.samples);
+  const user = useSelector((state) => state.user);
+  const [focusFilter, setFocusFilter] = useState("All");
+  const userLocal = JSON.parse(localStorage.getItem("user"));
 
   useEffect(() => {
-    if(user.length && !samples.length){
-      dispatch(getSamples(''))
-    }}, [dispatch, user])
-  
-  
+    if (samples === undefined) dispatch(getSamples(""));
+    if (userLocal && !user.length) {
+      dispatch(getEmployeeById(userLocal.SellerID));
+    }
+  });
 
-  if(userLocal){
-    if(user.length){
-      return(
+  useEffect(() => {
+    if (!samples.length) {
+      dispatch(getSamples(""));
+    }
+  }, []);
+
+  if (userLocal) {
+    if (user.length) {
+      return (
         <>
-          <SideBar user={user} focus={focus} setFocus={setFocus}/>
+          <SideBar user={user} focus={focus} setFocus={setFocus} />
           <SamplesContainer
-            samples={samples} 
-            user={user} 
-            focusFilter={focusFilter} 
-            setFocusFilter={setFocusFilter}/>
-        </>)
-        }else{
-          <Text>HOla</Text>
-        }      
-  }else return (
-    <>
-      <Redirect/>
-    </>
-    )
-  }
- 
+            samples={samples}
+            user={user}
+            focusFilter={focusFilter}
+            setFocusFilter={setFocusFilter}
+          />
+        </>
+      );
+    } else {
+      <Text>HOla</Text>;
+    }
+  } else
+    return (
+      <>
+        <Redirect />
+      </>
+    );
+};
 
-export default Samples
+export default Samples;
