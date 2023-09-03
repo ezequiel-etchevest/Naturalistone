a59b14c9fd01483f7a05d34a90b51e42bb0c491f
Eduardo Sequeira <eduardoasm19@gmail.com>
2023-09-03 18:10:25 -0400

    feature: add samples and tasks in stats

--- client/src/components/stats/PaymentsStats.js
+++ client/src/components/stats/PaymentsStats.js
@@ -1,91 +1,88 @@
 import {
-    Box,
-    Flex,
-    Stat,
-    StatLabel,
-    StatNumber,
-    HStack,
-    Spinner,
-    useDisclosure,
-  } from '@chakra-ui/react';
-import { BsCreditCardFill } from 'react-icons/bs';
-import { HiReceiptPercent } from 'react-icons/hi2';
-import { MdOutlinePayments } from 'react-icons/md';
-import PaymentsMadeModal from './paymentsMadeModal';
-  
-function StatsCard(props) {
+  Box,
+  Flex,
+  Stat,
+  StatLabel,
+  StatNumber,
+  HStack,
+  Spinner,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { BsCreditCardFill } from "react-icons/bs";
+import { FaRegClipboard } from "react-icons/fa";
+import { MdOutlinePayments } from "react-icons/md";
+import PaymentsMadeModal from "./paymentsMadeModal";
+import { BiTask } from "react-icons/bi";
 
-  const { isOpen: isOpen1, onOpen: onOpen1, onClose: onClose1 } = useDisclosure()
-    
+function StatsCard(props) {
+  const {
+    isOpen: isOpen1,
+    onOpen: onOpen1,
+    onClose: onClose1,
+  } = useDisclosure();
+
   const { title, stat, icon, visibility } = props;
 
   return (
     <>
-    <Stat
-    visibility={visibility}
-    w={'10vw'}
-    h={'16vh'}
-    px={{ base: 2, md: 4 }}
-    py={'5'}
-    border={'1px solid'}
-    borderColor={'web.border'}
-    rounded={'md'}
-    color={'web.text'}
-    _hover={{
-        bg: 'logo.orange',
-        color: 'web.text',
-        cursor: 'pointer',
-    }}
-    onClick={onOpen1}
-    >
-    <Flex justifyContent={'space-between'}>
-      <Box pl={{ base: 2, md: 6 }}>
-        <StatLabel fontWeight={'medium'} >
-          {title}
-        </StatLabel>
-        <StatNumber fontSize={'4xl'} fontWeight={'medium'}>
-          {stat?.toLocaleString('en-US')}
-        </StatNumber>
-      </Box>
-      <Box
-        my={'auto'}
-        color={'web.text'}
-        alignContent={'center'}
-        >
-        {icon}
-      </Box>
-    </Flex>
-  </Stat>
-  <PaymentsMadeModal isOpenModal={isOpen1} onCloseModal={onClose1} />
-  </>
-    );
-  }
-  
-export default function PaymentsStats({stats, filters}) {
+      <Stat
+        visibility={visibility}
+        w={"10vw"}
+        h={"16vh"}
+        px={{ base: 2, md: 4 }}
+        py={"5"}
+        border={"1px solid"}
+        borderColor={"web.border"}
+        rounded={"md"}
+        color={"web.text"}
+        _hover={{
+          bg: "logo.orange",
+          color: "web.text",
+          cursor: "pointer",
+        }}
+        onClick={onOpen1}
+      >
+        <Flex justifyContent={"space-between"}>
+          <Box pl={{ base: 2, md: 6 }}>
+            <StatLabel fontWeight={"medium"}>{title}</StatLabel>
+            <StatNumber fontSize={"4xl"} fontWeight={"medium"}>
+              {stat?.toLocaleString("en-US")}
+            </StatNumber>
+          </Box>
+          <Box my={"auto"} color={"web.text"} alignContent={"center"}>
+            {icon}
+          </Box>
+        </Flex>
+      </Stat>
+      <PaymentsMadeModal isOpenModal={isOpen1} onCloseModal={onClose1} />
+    </>
+  );
+}
 
-
+export default function PaymentsStats({ stats, filters }) {
   return (
-    <Box h={'92vh'} px={'4vw'} bg={'web.bg'} >
+    <Box h={"92vh"} px={"4vw"} bg={"web.bg"}>
       <HStack columns={{ base: 1, md: 3 }} spacing={{ base: 5, lg: 8 }}>
         <StatsCard
-        title={'Payments'}
-        stat={stats.TotalAmount === null ? 0 : Number(stats.TotalAmount.toFixed(2))}
-        icon={<MdOutlinePayments size={'3em'} />}
+          title={"Payments"}
+          stat={
+            stats.TotalAmount === null
+              ? 0
+              : Number(stats.TotalAmount.toFixed(2))
+          }
+          icon={<MdOutlinePayments size={"3em"} />}
         />
         <StatsCard
-        visibility='hidden'
-        display='none'
-        title={'Payments Amount'}
-        // stat={Number(paymentStats.totalCharged)}
-        icon={<BsCreditCardFill size={'3em'} />}
+          title={"Tasks Todo / Done"}
+          stat={`${stats.tasks[0].total_todo} / ${stats.tasks[0].total_done}`}
+          icon={<BiTask size={"3em"} />}
         />
         <StatsCard
-        visibility= 'hidden'
-        title={'Closing Days (Avg)'}
-        // stat={paymentStats.closingDaysAvg}
-        //icon={<HiReceiptPercent size={'3em'} />}
+          title={"Samples"}
+          stat={stats.samples[0].total_samples}
+          icon={<FaRegClipboard size={"3em"} />}
         />
       </HStack>
     </Box>
-    );
-  }
+  );
+}
